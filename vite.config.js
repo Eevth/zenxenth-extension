d55908eed49fe61b13e30db5dda41690ee2e70a8
Patih Nusantara <arcopolo2354@gmail.com>
2026-07-08 18:36:08 +0700

    Initial commit - Zenxenth mod

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
-  base: ''
-})
+  plugins: [react()],
+  base: "./",
+});
